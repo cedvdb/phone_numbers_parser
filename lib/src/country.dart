d3db5180e74041ef266415bee4d453d815f23480
cedvdb <cedvandenbosch@gmail.com>
2021-03-29 08:06:15 +0200

    country

--- lib/src/country.dart
+++ lib/src/country.dart
@@ -1,27 +1,60 @@
 class Country {
+  // English name of the country
+  final String name;
+
+  /// emoji flag
+  final String flag;
+  // short country code
   final String isoCode;
-  final String dialCode;
-  final Mobile mobile;
-  final String internationalPrefix;
-  final String? nationalPrefix;
-  final bool isMainCountryForIsoCode;
+  // description of what the phone number should look like
+  final PhoneDescription phone;
+
+  String get dialCode => phone.dialCode;
 
   const Country({
+    required this.name,
+    required this.flag,
     required this.isoCode,
-    required this.dialCode,
-    required this.mobile,
-    required this.internationalPrefix,
-    required this.nationalPrefix,
-    required this.isMainCountryForIsoCode,
+    required this.phone,
   });
 }
 
-class Mobile {
-  final String lengths;
+class PhoneDescription {
+  final String dialCode;
+  final String internationalPrefix;
+  final String? nationalPrefix;
+  final String? nationalPrefixTransformRule;
+  final bool? isMainCountryForIsoCode;
+  final PhoneValidation validation;
+
+  const PhoneDescription({
+    required this.dialCode,
+    required this.internationalPrefix,
+    this.nationalPrefix,
+    this.nationalPrefixTransformRule,
+    this.isMainCountryForIsoCode,
+    required this.validation,
+  });
+}
+
+class PhoneValidation {
+  final PhoneValidationRules general;
+  final PhoneValidationRules mobile;
+  final PhoneValidationRules fixedLine;
+
+  const PhoneValidation({
+    required this.general,
+    required this.mobile,
+    required this.fixedLine,
+  });
+}
+
+class PhoneValidationRules {
+  final String? lengths;
   final String pattern;
 
-  Mobile({
-    required this.lengths,
+  const PhoneValidationRules({
+    this.lengths,
     required this.pattern,
   });
 }
