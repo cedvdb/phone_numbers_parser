b5c530fafce458b6f65eeae3a92f233ecbb9fc34
cedvdb <cedvandenbosch@gmail.com>
2021-03-31 08:53:12 +0200

    base files

---
+++ lib/resources/file_generation/base_country_by_iso_code.dart
@@ -0,0 +1,8 @@
+import 'package:phone_numbers_parser/src/models/country.dart';
+import 'package:phone_numbers_parser/src/models/phone_description.dart';
+
+// comment here
+
+Map<String, Country> countriesByDialCode = {
+// data here
+};
---
+++ lib/resources/file_generation/base_country_list.dart
@@ -0,0 +1,8 @@
+import 'package:phone_numbers_parser/src/models/country.dart';
+import 'package:phone_numbers_parser/src/models/phone_description.dart';
+
+// comment here
+
+List<Country> countries = [
+// data here
+];
