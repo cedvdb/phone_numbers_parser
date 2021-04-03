2e77cca9bb7e8a542b67e27deea842b7cadfeeca
cedvdb <cedvandenbosch@gmail.com>
2021-04-03 23:23:00 +0200

    fix possible phone number pattenr

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -14,22 +14,27 @@ class Constants {
   static final int MIN_LENGTH_COUNTRY_PLUS_NSN = 5;
 
   /// accepted punctuation within a phone number
-  static final String _PUNCTUATION = ' ()[\]\-\./\\';
-  static final String _PLUS = '+＋';
+  static final String _PUNCTUATION = r' ()\[\]\-\.\/\\';
+  static final String _PLUS = r'+＋';
 
   /// Westhen and easthern arabic numerals
-  static final String _DIGITS = '0-9０-９٠-٩۰-۹';
+  static final String _DIGITS = r'0-9０-９٠-٩۰-۹';
 
   /// Regex to find possible phone number candidates in a string.
   ///
-  /// This regex tries to match all *mobile* phone numbers. It doesn't match special
+  /// This regex tries to match all phone numbers. It doesn't match special
   /// numbers like the 100.
   ///
   /// The regex must start by either a + or a digit, then be followed by at least 6 digits.
   /// The digits are formed of westhern and easthern numerals.
   /// There can also be punctuation between the digts.
   static final String POSSIBLE_PHONE_NUMBER =
-      '[$_PLUS$_DIGITS](?:[$_PUNCTUATION]{0,5}$_DIGITS){6,}';
+      '[$_PLUS$_DIGITS](?:[$_PUNCTUATION]{0,3}[$_DIGITS]){6,}';
+
+  /// Matches strings that look like dates using "/" as a separator. Examples: 3/10/2011, 31/10/96 or
+  /// 08/31/95.
+  static final Pattern SLASH_SEPARATED_DATES =
+      r'(?:(?:[0-3]?\\d/[01]?\\d)|(?:[01]?\\d/[0-3]?\\d))/(?:[12]\\d)?\\d{2}';
   // Replace Easthern to Westhern arabic numbers https://en.wikipedia.org/wiki/Eastern_Arabic_numerals
   static Map<String, String> allNormalizationMappings = {
     '+': '+',
