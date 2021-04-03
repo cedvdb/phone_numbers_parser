5d6b8592fb949262e5d10787f563ec1138791754
cedvdb <cedvandenbosch@gmail.com>
2021-04-03 12:23:08 +0200

    export necessary files

--- lib/phone_numbers_parser.dart
+++ lib/phone_numbers_parser.dart
@@ -1,4 +1,7 @@
 library phone_numbers_parser;
 
 export 'src/exceptions.dart';
-export 'src/parsers/parser.dart';
+export 'src/generated/country_list.dart';
+export 'src/models/country.dart';
+export 'src/models/phone_number.dart';
+export 'src/parsers/phone_number_parser.dart';
