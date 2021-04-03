8e570df399de980bbba583b0e89d28c81cb3b466
cedvdb <cedvandenbosch@gmail.com>
2021-04-03 23:22:20 +0200

    country test pass

---
+++ test/country_test.dart
@@ -0,0 +1,21 @@
+// test that
+
+import 'package:phone_numbers_parser/phone_numbers_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Country', () {
+    test('should create with isoCode ', () {
+      expect(Country.fromIsoCode('fr').name, equals('France'));
+      expect(Country.fromIsoCode('FR').name, equals('France'));
+      expect(() => Country.fromIsoCode('WRONG'),
+          throwsA(TypeMatcher<PhoneNumberException>()));
+    });
+    test('should create with isoCode ', () {
+      expect(Country.fromDialCode('33').name, equals('France'));
+      expect(Country.fromDialCode('+33').name, equals('France'));
+      expect(() => Country.fromDialCode('0'),
+          throwsA(TypeMatcher<PhoneNumberException>()));
+    });
+  });
+}
