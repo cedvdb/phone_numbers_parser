8a466cb29e69868458d5ae4d9709ae1e40276116
cedvdb <cedvandenbosch@gmail.com>
2021-04-07 16:23:13 +0200

    correct matchEntirely

--- lib/src/regexp_fix.dart
+++ lib/src/regexp_fix.dart
@@ -1,9 +1,10 @@
 // extension on regexp until they implement it https://github.com/dart-lang/sdk/issues/45586
 extension MatchEntirely on RegExp {
-  Match? matchEntirely(String pattern) {
+  Match? matchEntirely(String string) {
+    var pattern = this.pattern;
     if (!pattern.endsWith(r'$')) {
       pattern = pattern + r'$';
     }
-    return matchAsPrefix(pattern);
+    return RegExp(pattern).matchAsPrefix(string);
   }
 }
