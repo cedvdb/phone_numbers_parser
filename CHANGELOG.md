5ada9663c1560f495941a7cc2af550961fe22b8b
cedvdb <cedvandenbosch@gmail.com>
2021-11-07 19:13:04 +0100

    Validate length fix #18 (#19)
    * 4.1.0 back substitution when invalid (#16)
    
    * Only modify national number to its international version when it is valid (#14)
    
    * only modify phone number when valid
    
    * valid modif changelog
    
    * slight modif on operator for future local
    
    * new version number
    
    * invalid backsubstitution (#15)
    
    * 4.1.0 (#17)
    
    * Only modify national number to its international version when it is valid (#14)
    
    * only modify phone number when valid
    
    * valid modif changelog
    
    * slight modif on operator for future local
    
    * new version number
    
    * invalid backsubstitution (#15)
    
    * fix invalid validation
    
    * remove print

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 4.2.0
+  - Fix incorrect parsing in some cases
+  - fix validate length as it was incorrectly validating with pattern
+
 ## 4.1.0
   - only modify national number to its international version when it is valid
   - resolve issue where phone number were incorrectly parsed if national number started with country code (it happens)
--- test/src/parsers/phone_number_test.dart
+++ test/src/parsers/phone_number_test.dart
@@ -7,5 +7,24 @@ void main() {
       expect(PhoneNumber(isoCode: '33', nsn: '479999999'),
           equals(PhoneNumber(isoCode: '33', nsn: '479999999')));
     });
+    test('creation', () {
+      final phone1 = PhoneNumber.fromIsoCode('GB', '7000000000');
+      final phone2 = PhoneNumber.fromCountryCode('44', '7000000000');
+      final phone3 = PhoneNumber.fromRaw('+447000000000');
+      expect((phone1 == phone2 && phone1 == phone3), isTrue);
+    });
+
+    test('validity', () {
+      final gbValidLength = PhoneNumber.fromIsoCode('GB', '0000000000');
+      expect(gbValidLength.validateLength(), isTrue);
+      expect(gbValidLength.validate(type: PhoneNumberType.mobile), isFalse);
+      expect(gbValidLength.validate(type: PhoneNumberType.fixedLine), isFalse);
+      expect(gbValidLength.validate(), isFalse);
+      final gbValidPattern = PhoneNumber.fromIsoCode('GB', '7111111111');
+      expect(gbValidPattern.validateLength(), isTrue);
+      expect(gbValidPattern.validate(), isTrue);
+    });
   });
+
+  group('zeroes', () {});
 }
