e256bf033ab9d80f4e589dbd7ee6b4a564f19756
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-10-02 21:39:12 +0530

    name changed

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -2,25 +2,25 @@ import 'package:phone_numbers_parser/src/metadata/generated/metadata_lengths_by_
 import 'package:phone_numbers_parser/src/models/iso_code.dart';
 import 'package:phone_numbers_parser/src/models/phone_number_type.dart';
 
-class MaxMinUtils{
-  static MaxMinLength getMaxMinLengthByIsoCode(IsoCode isoCode, PhoneNumberType phoneNumberType){
+class MinMaxUtils{
+  static MinMaxLength getMaxMinLengthByIsoCode(IsoCode isoCode, PhoneNumberType phoneNumberType){
     if(phoneNumberType == PhoneNumberType.mobile) {
      final data = metadataLenghtsByIsoCode[isoCode]!.mobile;
      if(data.isNotEmpty){
-       return MaxMinLength(data.first, data.last);
+       return MinMaxLength(data.first, data.last);
      }
     }else if(phoneNumberType == PhoneNumberType.fixedLine){
   final data = metadataLenghtsByIsoCode[isoCode]!.fixedLine;
      if(data.isNotEmpty){
-       return MaxMinLength(data.first, data.last);
+       return MinMaxLength(data.first, data.last);
      }
     }
-    return MaxMinLength(15, 15);
+    return MinMaxLength(15, 15);
   } 
 }
 
-class MaxMinLength{
+class MinMaxLength{
   int maxLength;
   int minLength;
-  MaxMinLength(this.minLength, this.maxLength);
+  MinMaxLength(this.minLength, this.maxLength);
 }
